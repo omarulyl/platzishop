3cb01c468e69e9ec4fb1b7354058775bfd171a3f
Omar <omaru.lyl@gmail.com>
2017-03-31 11:13:42 -0600

    Se agrego html de catalago y funcion de ventana modal al ingresar

--- js/app.js
+++ js/app.js
@@ -20,3 +20,14 @@ $form.submit((ev) => {
   el.addClass('form-control-feedback')
   $formGroup.append(el)
 })
+
+function mostrarModal(){
+	const noMostrarModal = JSON.parse(localStorage.noMostrarModal)
+	if (!noMostrarModal) {
+		$('#modalOferta').modal()
+	}
+
+	$('#btnNoRegistrar').click(function (ev){
+		localStorage.noMostrarModal = true
+	})
+}
