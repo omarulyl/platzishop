d443c3bfdf23a8931b7e8c84e29be9e50c65d175
Omar <omaru.lyl@gmail.com>
2017-03-31 14:31:47 -0600

    Se agrego filtros en la parte de catalago del site

--- js/app.js
+++ js/app.js
@@ -31,3 +31,15 @@ function mostrarModal(){
 		localStorage.noMostrarModal = true
 	})
 }
+
+const $filtrosToggle = $('#filtrosToggle')
+$filtrosToggle.click(function(ev){
+	ev.preventDefault() //evita que se ejecute el href
+	const $i = $filtrosToggle.find('i.fa')
+	const isdown = $i.hasClass('fa-chevron-down')
+	if (isdown) {
+		$i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+	} else {
+		$i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+	}
+})
